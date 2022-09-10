06f491c7848ee08affd9884ea30528a5df74d664
shashicode2n4mcoffee <shahsicode2n4mcoffee@gmail.com>
2022-09-10 14:28:41 +0530

    Register module initial stage

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [RegisterComponent],
+})
+export class AuthModule {}
